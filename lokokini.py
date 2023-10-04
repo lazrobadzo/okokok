037ecb399d9f8995843f79776aac07fd50a2da64
lazrobadzo <144172921+lazrobadzo@users.noreply.github.com>
2023-10-04 08:21:44 -0700

    Add files via upload

---
+++ lokokini.py
@@ -0,0 +1,24 @@
+user_age = input ("what your age:? my age is 22")
+
+print(user_age)
+
+user_name =input ("what is your name:? my name is lazare")
+
+print(user_name)
+
+usermom_name = input ("mom what is your name:? mom name is oto")
+
+print(usermom_name)
+
+userded_name= input ("ded what is your name:? ded name is lizi")
+
+print(userded_name)
+
+my_age=15
+mom_age=35
+ded_age=40
+
+
+print(my_age+5)
+print(mom_age+5)
+print(ded_age+5)
